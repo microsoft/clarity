5d9bedce22cf7bd20c306696d722bfd5300ec1b8
swaathee <swaathee@users.noreply.github.com>
2020-09-22 16:12:10 -0700

    Swradhak/baselinebugfix (#41)
    * bug fix - buffer not deep copied to state
    
    * minor change

--- packages/clarity-js/src/data/baseline.ts
+++ packages/clarity-js/src/data/baseline.ts
@@ -15,7 +15,22 @@ export function start(): void {
 export function reset(): void {
     // Baseline state holds the previous values - if it is updated in the current payload,
     // reset the state to current value after sending the previous state
-    state = update ? { time: time(), event: Event.Baseline, data: buffer } : state;
+    state = update ?
+        { time: time(),
+          event: Event.Baseline,
+          data: {
+            visible: buffer.visible,
+            docWidth: buffer.docWidth,
+            docHeight: buffer.docHeight,
+            screenWidth: buffer.screenWidth,
+            screenHeight: buffer.screenHeight,
+            scrollX: buffer.scrollX,
+            scrollY: buffer.scrollY,
+            pointerX: buffer.pointerX,
+            pointerY: buffer.pointerY,
+            activityTime: buffer.activityTime
+          }
+        } : state;
     buffer = buffer ? buffer : {
         visible: BooleanFlag.True,
         docWidth: 0,
