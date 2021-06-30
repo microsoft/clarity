a9183c8afabb987671494f5170fcee51d8a6529c
Sarvesh Nagpal <sarveshnagpal@gmail.com>
2021-06-29 18:21:00 -0700

    Adding try catch around compression code

--- packages/clarity-js/src/data/compress.ts
+++ packages/clarity-js/src/data/compress.ts
@@ -3,15 +3,17 @@ import { Constant } from "@clarity-types/data";
 const supported = Constant.CompressionStream in window;
 
 export default async function(input: string): Promise<Uint8Array> {
-    if (supported) {
-        // Create a readable stream from given input string and
-        // pipe it through text encoder and compression stream to gzip it
-        const stream = new ReadableStream({async start(controller) {
-            controller.enqueue(input);
-            controller.close();
-        }}).pipeThrough(new TextEncoderStream()).pipeThrough(new window[Constant.CompressionStream]("gzip"));
-        return new Uint8Array(await read(stream));
-    }
+    try {
+        if (supported) {
+            // Create a readable stream from given input string and
+            // pipe it through text encoder and compression stream to gzip it
+            const stream = new ReadableStream({async start(controller) {
+                controller.enqueue(input);
+                controller.close();
+            }}).pipeThrough(new TextEncoderStream()).pipeThrough(new window[Constant.CompressionStream]("gzip"));
+            return new Uint8Array(await read(stream));
+        }
+    } catch { /* do nothing */ }
     return null;
 }
 
