13e2030284999303817929e3acceac5f0ca801a0
Mostafa Alaa <mostafa-alaa@outlook.com>
2022-09-06 12:28:56 +0200

    fixed an issue where in some cases the callback function in the timeout is passed without the arguments

--- packages/clarity-js/src/core/timeout.ts
+++ packages/clarity-js/src/core/timeout.ts
@@ -2,7 +2,7 @@ import { Event } from "@clarity-types/data";
 import measure from "./measure";
 
 export function setTimeout(handler: (event?: Event | boolean) => void, timeout?: number, event?: Event): number {
-    return window.setTimeout(measure(handler), timeout, event);
+    return window.setTimeout(() => measure(handler)(event), timeout);
 }
 
 export function clearTimeout(handle: number): void {
