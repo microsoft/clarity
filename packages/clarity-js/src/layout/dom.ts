baeef3f045e83d1c0c65ee2ef3a486bdd8d7dbe8
Sam Martin <samart@microsoft.com>
2022-10-12 11:05:02 -0700

    cruncher removes conditoinal operator

--- packages/clarity-js/src/layout/dom.ts
+++ packages/clarity-js/src/layout/dom.ts
@@ -310,7 +310,7 @@ function updateSelector(value: NodeValue): void {
 export function hashText(hash: string): string {
     let id = lookup(hash);
     let node = getNode(id);
-    return node?.textContent;
+    return node ? node.textContent : '';
 }
 
 export function getNode(id: number): Node {
