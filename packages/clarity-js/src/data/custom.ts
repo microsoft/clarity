8dac26da119a233b56d90ed347559410c97a2f0a
Sam Martin <samart@microsoft.com>
2023-10-04 10:01:32 -0700

    adding clarifying comment

--- packages/clarity-js/src/data/custom.ts
+++ packages/clarity-js/src/data/custom.ts
@@ -4,6 +4,8 @@ import encode from "./encode";
 
 export let data: CustomData = null;
 
+// custom events allow 2 parameters or 1 parameter to be passed. If 2 are passed we
+// consider it a key value pair. If only 1 is passed we only consider the event to have a value.
 export function event(a: string, b: string): void {
     if (core.active() &&
         a &&
