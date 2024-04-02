ad24e100bee2cf1006d35599f7387fe7fe36db93
Abdelrhmanmagdy <abdelrhman.magdy96@gmail.com>
2024-04-03 00:31:50 +0200

    fixing format

--- packages/clarity-js/src/interaction/resize.ts
+++ packages/clarity-js/src/interaction/resize.ts
@@ -1,7 +1,7 @@
-import { Event } from '@clarity-types/data';
-import { ResizeData } from '@clarity-types/interaction';
-import { bind } from '@src/core/event';
-import encode from './encode';
+import { Event } from "@clarity-types/data";
+import { ResizeData } from "@clarity-types/interaction";
+import { bind } from "@src/core/event";
+import encode from "./encode";
 
 export let data: ResizeData;
 let debounceTimeoutId: number;
@@ -12,20 +12,14 @@ export function start(): void {
 }
 
 function recompute(): void {
-  let de = document.documentElement;
-  // window.innerWidth includes width of the scrollbar and is not a true representation of the viewport width.
-  // Therefore, when possible, use documentElement's clientWidth property.
-  data = {
-    width:
-      de && 'clientWidth' in de
-        ? Math.min(de.clientWidth, window.innerWidth)
-        : window.innerWidth,
-    height:
-      de && 'clientHeight' in de
-        ? Math.min(de.clientHeight, window.innerHeight)
-        : window.innerHeight,
-  };
-  encode(Event.Resize);
+    let de = document.documentElement;
+    // window.innerWidth includes width of the scrollbar and is not a true representation of the viewport width.
+    // Therefore, when possible, use documentElement's clientWidth property.
+    data = {
+        width: de && "clientWidth" in de ? Math.min(de.clientWidth, window.innerWidth) : window.innerWidth,
+        height: de && "clientHeight" in de ? Math.min(de.clientHeight, window.innerHeight) : window.innerHeight,
+    };
+    encode(Event.Resize);
 }
 
 export function reset(): void {
@@ -44,4 +38,4 @@ export function debounce(callback: Function, delay: number) {
       clearTimeout(debounceTimeoutId);
       debounceTimeoutId = setTimeout(callback, delay);
     };
-  }
+}
