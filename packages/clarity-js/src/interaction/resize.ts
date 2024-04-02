0f03d3a4969d8e86fe93caf3e164de9f297e9918
Abdelrhmanmagdy <abdelrhman.magdy96@gmail.com>
2024-04-02 17:26:31 +0200

    Debouncing resize event

--- packages/clarity-js/src/interaction/resize.ts
+++ packages/clarity-js/src/interaction/resize.ts
@@ -1,30 +1,47 @@
-import { Event } from "@clarity-types/data";
-import { ResizeData } from "@clarity-types/interaction";
-import { bind } from "@src/core/event";
-import encode from "./encode";
+import { Event } from '@clarity-types/data';
+import { ResizeData } from '@clarity-types/interaction';
+import { bind } from '@src/core/event';
+import encode from './encode';
 
 export let data: ResizeData;
+let debounceTimeoutId: number;
 
 export function start(): void {
-    bind(window, "resize", recompute);
-    recompute();
+  bind(window, 'resize', debounce(recompute, 500));
+  recompute();
 }
 
 function recompute(): void {
-    let de = document.documentElement;
-    // window.innerWidth includes width of the scrollbar and is not a true representation of the viewport width.
-    // Therefore, when possible, use documentElement's clientWidth property.
-    data = {
-        width: de && "clientWidth" in de ? Math.min(de.clientWidth, window.innerWidth) : window.innerWidth,
-        height: de && "clientHeight" in de ? Math.min(de.clientHeight, window.innerHeight) : window.innerHeight,
-    };
-    encode(Event.Resize);
+  let de = document.documentElement;
+  // window.innerWidth includes width of the scrollbar and is not a true representation of the viewport width.
+  // Therefore, when possible, use documentElement's clientWidth property.
+  data = {
+    width:
+      de && 'clientWidth' in de
+        ? Math.min(de.clientWidth, window.innerWidth)
+        : window.innerWidth,
+    height:
+      de && 'clientHeight' in de
+        ? Math.min(de.clientHeight, window.innerHeight)
+        : window.innerHeight,
+  };
+  encode(Event.Resize);
 }
 
 export function reset(): void {
-    data = null;
+  data = null;
+  if(debounceTimeoutId){
+      clearTimeout(debounceTimeoutId);
+  }
 }
 
 export function stop(): void {
-    reset();
+  reset();
 }
+
+export function debounce(callback: Function, delay: number) {
+    return function () {
+      clearTimeout(debounceTimeoutId);
+      debounceTimeoutId = setTimeout(callback, delay);
+    };
+  }
