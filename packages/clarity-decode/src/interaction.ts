e500e84bf38fb0a6ff30da26fce5cd9afd5dba47
Zoe Yang <zoeyang@microsoft.com>
2024-04-24 16:36:37 -0700

    remove brackets

--- packages/clarity-decode/src/interaction.ts
+++ packages/clarity-decode/src/interaction.ts
@@ -73,8 +73,8 @@ export function decode(tokens: Data.Token[]): InteractionEvent {
                 target: tokens[2] as number,
                 x: tokens[3] as number,
                 y: tokens[4] as number,
-                top: tokens.length > 5 ? (tokens[5] as string) : null,
-                bottom: tokens.length > 6 ? (tokens[6] as string) : null
+                top: tokens.length > 5 ? tokens[5] as string : null,
+                bottom: tokens.length > 6 ? tokens[6] as string : null
             };
             return { time, event, data: scrollData };
         case Data.Event.Timeline:
