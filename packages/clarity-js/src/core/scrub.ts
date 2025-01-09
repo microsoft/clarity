20e4c61b867c66e058baeb8172679fca54180d4e
alaamegahed <alaamegahed@microsoft.com>
2025-01-09 13:07:45 +0200

    update threshold

--- packages/clarity-js/src/core/scrub.ts
+++ packages/clarity-js/src/core/scrub.ts
@@ -4,7 +4,7 @@ import * as Layout from "@clarity-types/layout";
 import config from "@src/core/config";
 
 const catchallRegex = /\S/gi;
-const maxUrlLength = 100; // this is an arbitrary number to truncate URLs sent with every payload, can be changed later
+const maxUrlLength = 255;
 let unicodeRegex = true;
 let digitRegex = null;
 let letterRegex = null;
