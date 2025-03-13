cf2d2814d08d664a3ff80ccad41e0b4cb5f55487
Roman Galeev <romangaleev@microsoft.com>
2025-03-13 00:33:59 -0700

    stub tests to use in the release workflow

---
+++ packages/clarity-js/test/stub.test.ts
@@ -0,0 +1,7 @@
+import { assert } from 'chai';
+
+describe('Stub Tests! Replace with working real tests!', () => {
+    it('should be a successful test', () => {
+        assert.isTrue(true);
+    });
+});
