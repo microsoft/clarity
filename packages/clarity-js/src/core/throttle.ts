c7db47146ccaefbd902fa0b11b7cfd2906faf228
Manu Nair <manunair@microsoft.com>
2025-05-01 23:51:29 -0700

    fix typo

--- packages/clarity-js/src/core/throttle.ts
+++ packages/clarity-js/src/core/throttle.ts
@@ -4,7 +4,7 @@ export default function throttle<T extends (...args: any[]) => void>(func: T, du
   return function (...args: Parameters<T>) {
     const now = performance.now();
 
-    // Execute immediaately for first instance, or after specified duration has elapsed
+    // Execute immediately for first instance, or after specified duration has elapsed
     if (lastExecutionTime === 0 || now - lastExecutionTime >= duration) {
       lastExecutionTime = now;
       func.apply(this, args);
