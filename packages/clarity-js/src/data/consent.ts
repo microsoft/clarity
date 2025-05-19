4ae1c78b8dda3be87c838edcaa1b6da76637b3b6
Sam Martin <samart@microsoft.com>
2025-05-19 10:27:40 -0700

    consent and custom

--- packages/clarity-js/src/data/consent.ts
+++ packages/clarity-js/src/data/consent.ts
@@ -1,10 +1,10 @@
 import { Dimension } from "@clarity-types/data";
 import * as dimension from "@src/data/dimension";
 
-const enum ConsentType {
+enum ConsentType {
     None = 0,
     Implicit = 1,
-    General = 2
+    General = 2,
 }
 
 export function config(track: boolean): void {
