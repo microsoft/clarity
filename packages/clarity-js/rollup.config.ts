656b55bdda9ab25515630be4f7ff76cba8737ce6
Seena Augusty <seenaaugusty@microsoft.com>
2025-06-10 11:10:24 -0700

    Build on different targets ES2017 and ES5

--- packages/clarity-js/rollup.config.ts
+++ packages/clarity-js/rollup.config.ts
@@ -4,14 +4,18 @@ import resolve from "@rollup/plugin-node-resolve";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import pkg from "./package.json" assert { type: "json" };
+import path from "path";
+const target = process.env.TARGET || "es2017";
+const outDir = process.env.OUTDIR || "build";
+
 export default [
     {
         input: "src/index.ts",
         output: [
-            { file: pkg.main, format: "cjs", exports: "named" },
-            { file: pkg.module, format: "es", exports: "named" },
+            { file: path.join(outDir, path.basename(pkg.main)), format: "cjs", exports: "named" },
+            { file: path.join(outDir, path.basename(pkg.module)), format: "es", exports: "named" },
         ],
-        plugins: [resolve(), typescript(), commonjs({ include: ["node_modules/**"] })],
+        plugins: [resolve(), typescript({ target: target }), commonjs({ include: ["node_modules/**"] })],
         onwarn(message, warn) {
             if (message.code === "CIRCULAR_DEPENDENCY") {
                 return;
@@ -21,29 +25,29 @@ export default [
     },
     {
         input: "src/global.ts",
-        output: [{ file: pkg.unpkg, format: "iife", exports: "named" }],
+        output: [{ file: path.join(outDir, path.basename(pkg.unpkg)), format: "iife", exports: "named" }],
         onwarn(message, warn) {
             if (message.code === "CIRCULAR_DEPENDENCY") {
                 return;
             }
             warn(message);
         },
-        plugins: [, resolve(), typescript(), terser({ output: { comments: false } }), commonjs({ include: ["node_modules/**"] })],
+        plugins: [, resolve(), typescript({ target: target }), terser({ output: { comments: false } }), commonjs({ include: ["node_modules/**"] })],
     },
     {
         input: "src/global.ts",
-        output: [{ file: pkg.extended, format: "iife", exports: "named" }],
+        output: [{ file: path.join(outDir, path.basename(pkg.extended)), format: "iife", exports: "named" }],
         onwarn(message, warn) {
             if (message.code === "CIRCULAR_DEPENDENCY") {
                 return;
             }
             warn(message);
         },
-        plugins: [resolve(), typescript(), terser({ output: { comments: false } }), commonjs({ include: ["node_modules/**"] })],
+        plugins: [resolve(), typescript({ target: target }), terser({ output: { comments: false } }), commonjs({ include: ["node_modules/**"] })],
     },
     {
         input: "src/global.ts",
-        output: [{ file: pkg.insight, format: "iife", exports: "named" }],
+        output: [{ file: path.join(outDir, path.basename(pkg.insight)), format: "iife", exports: "named" }],
         onwarn(message, warn) {
             if (message.code === "CIRCULAR_DEPENDENCY") {
                 return;
@@ -62,14 +66,14 @@ export default [
                 ],
             }),
             resolve(),
-            typescript(),
+            typescript({ target: target }),
             terser({ output: { comments: false } }),
             commonjs({ include: ["node_modules/**"] }),
         ],
     },
     {
         input: "src/global.ts",
-        output: [{ file: pkg.performance, format: "iife", exports: "named" }],
+        output: [{ file: path.join(outDir, path.basename(pkg.performance)), format: "iife", exports: "named" }],
         onwarn(message, warn) {
             if (message.code === "CIRCULAR_DEPENDENCY") {
                 return;
@@ -86,7 +90,7 @@ export default [
                 ],
             }),
             resolve(),
-            typescript(),
+            typescript({ target: target }),
             terser({ output: { comments: false } }),
             commonjs({ include: ["node_modules/**"] }),
         ],
