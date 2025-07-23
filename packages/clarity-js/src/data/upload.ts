842fbeddd196d6b73156fd006cfcd07ecf7c2bcc
Manu Nair <manunair@microsoft.com>
2025-07-23 11:47:17 -0700

    Add support for snapshot trigger in response function

--- packages/clarity-js/src/data/upload.ts
+++ packages/clarity-js/src/data/upload.ts
@@ -20,6 +20,7 @@ import * as extract from "@src/data/extract";
 import * as style from "@src/layout/style";
 import { report } from "@src/core/report";
 import { signalsEvent } from "@src/data/signal";
+import { snapshot } from "@src/insight/snapshot";
 
 let discoverBytes: number = 0;
 let playbackBytes: number = 0;
@@ -301,6 +302,9 @@ function response(payload: string): void {
                 break;
             case Constant.Signal:
                 if (parts.length > 1) { signalsEvent(parts[1]); }
+                break;
+            case Constant.Snapshot:
+                snapshot();
                 break;
         }
     }
