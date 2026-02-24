d541a5de8dec73eadb9cff8d37a6384515bb9f73
Moemen Ahmed <moemenahmed@microsoft.com>
2026-02-24 12:36:37 +0200

    Use numeric enum

--- packages/clarity-js/types/brand-agent.d.ts
+++ packages/clarity-js/types/brand-agent.d.ts
@@ -1,11 +1,11 @@
 // This should exactly match with Agent JS.
 
 export enum BrandAgentEventName {
-  BubbleShown = "BubbleShown",
-  NudgeClicked = "NudgeClicked",
-  AgentMessageSent = "AgentMessageSent",
-  UserMessageSent = "UserMessageSent",
-  ConversationStarted = "ConversationStarted",
+  BubbleShown,
+  NudgeClicked,
+  AgentMessageSent,
+  UserMessageSent,
+  ConversationStarted,
 }
 
 export interface BrandAgentData {
